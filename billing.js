54b88e76c8c5a3e93071b3b275734225cd602356
MCFrank16 <boonatechnologies@gmail.com>
2020-08-28 18:32:49 +0200

    handling cors errors

--- billing.js
+++ billing.js
@@ -6,10 +6,8 @@ export const main = handler(async(event, context) => {
     const { storage, source } = JSON.parse(event.body);
     const amount = calculateCost(storage);
     const description = "Scratch charge";
-    
     // a new stripe object using our stripe secret key.
     const stripe = stripePackage(process.env.stripeSecretKey);
-    
     // a method to charge a user and respond to the request if everything went through successfully.
     await stripe.charges.create({
         source,
